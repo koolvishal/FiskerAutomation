0a39fa9139e0e7a6176b81740459d1bbd6cc59c3
koolvishal <vishalkulkarni21@gmail.com>
2023-03-30 23:18:12 +0530

    commit for Extent Report,test data utility

--- src/main/java/com/qa/DPS/Factory/PlaywrightFactory.java
+++ src/main/java/com/qa/DPS/Factory/PlaywrightFactory.java
@@ -3,6 +3,8 @@ package com.qa.DPS.Factory;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Properties;
 
 import com.microsoft.playwright.Browser;
@@ -19,36 +21,64 @@ public class PlaywrightFactory {
 	static Page page;
 	Properties prop;
 	
+	private static ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
+	private static ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
+	private static ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
+	private static ThreadLocal<Page> tlPage = new ThreadLocal<>();
+	
+	public static Playwright getPlaywright()
+	{
+		return tlPlaywright.get();
+	}
+	
+	public static Browser getBrowser()
+	{
+		return tlBrowser.get();
+	}
+	
+	public static BrowserContext getBrowserContext()
+	{
+		return tlBrowserContext.get();
+	}
+	
+	public static Page getPage()
+	{
+		return tlPage.get();
+	}
+	
+	
 	public Page init(Properties prop)
 	{
 		String strBrowser = prop.getProperty("browser").trim();
-		playwright = Playwright.create();
+		//playwright = Playwright.create();
+		tlPlaywright.set(Playwright.create());
 		 switch (strBrowser) 
 		 {
 		 case "chrome":
 			 LaunchOptions lp = new LaunchOptions();
 			 lp.setChannel("chrome");
 			 lp.setHeadless(false);
-			 browser = playwright.chromium().launch(lp);
+			 //browser = playwright.chromium().launch(lp);
+			 tlBrowser.set(getPlaywright().chromium().launch(lp));
 			 break;
-			 //browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
-			 
-			 
 		 case "firefox":
 			 LaunchOptions lp1 = new LaunchOptions();
 			 lp1.setChannel("firefox");
 			 lp1.setHeadless(false);
-			 browser = playwright.firefox().launch(lp1);
+			 //browser = playwright.firefox().launch(lp1);
+			 tlBrowser.set(playwright.firefox().launch(lp1));
 			 break;
 		 }
 		 
-		 browserContext = browser.newContext(new Browser.NewContextOptions().setHttpCredentials(prop.getProperty("username").trim(), prop.getProperty("password").trim()));
-		 //browserContext = browser.newContext();
-		 page = browserContext.newPage();
+		 //browserContext = browser.newContext(new Browser.NewContextOptions().setHttpCredentials(prop.getProperty("username").trim(), prop.getProperty("password").trim()));
+		 tlBrowserContext.set(getBrowser().newContext(new Browser.NewContextOptions().setHttpCredentials(prop.getProperty("username").trim(), prop.getProperty("password").trim())));
+		 //page = browserContext.newPage();
+		 tlPage.set(getBrowserContext().newPage());
 		 String strURL = prop.getProperty("url");
-		 page.navigate(strURL);
-		 
-		 return page;
+		// page.navigate(strURL);
+		 getPage().navigate(strURL);
+		 //return page;
+		 return getPage();
 		 
 		 
 	}
@@ -69,6 +99,16 @@ public class PlaywrightFactory {
 		
 		return prop;
 		
+	}
+	
+	public static String takeScreenshot() {
+		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+		//getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+		
+		byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+		String base64Path = Base64.getEncoder().encodeToString(buffer);
+		
+		return base64Path;
 	}
 	
 	
--- src/test/java/com/qa/DPS/Tests/CreatereservationpageTest.java
+++ src/test/java/com/qa/DPS/Tests/CreatereservationpageTest.java
@@ -1,28 +1,55 @@
 package com.qa.DPS.Tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.qa.DPS.Utilities.*;
 import com.qa.DPS.Base.BaseTest;
 
 public class CreatereservationpageTest extends BaseTest {
 
 	
-	@Test(priority=1)
-	public void VerifyCreateReservation()
+	@Test(dataProvider="createReservationtestData",priority=1)
+	public void VerifyCreateReservation(String cardowner,String cardno,String expdate,String CVC,String zip)
 	{
 		  loginpage.clickAccount();
-		  homegpage=loginpage.Login("automation2@qa.com", "Test@1234");
+		  homegpage=loginpage.Login("automation2@qa.com","Test@1234");
 		  createreservationpage=homegpage.clickReserve(); 
-		  createreservationpage.EnterNameOfCardOwner("autousa");
-		  createreservationpage.EnterCardNo("4242424242424242");
-		  createreservationpage.EnterExpDate("06/24");
-		  createreservationpage.EnterCVC("123");
-		  createreservationpage.EnterBillingZip("99999");
+		  createreservationpage.EnterNameOfCardOwner(cardowner);
+		  createreservationpage.EnterCardNo(cardno);
+		  createreservationpage.EnterExpDate(expdate);
+		  createreservationpage.EnterCVC(CVC);
+		  createreservationpage.EnterBillingZip(zip);
 		  createreservationpage.ClickConfirm();
 		  String act = createreservationpage.getResSuccessMsg();
 		  Assert.assertEquals(act, "Your First Fisker Ocean is now Reserved!");
-		  
+
+	}
+	
+	@DataProvider(name="createReservationtestData")
+	public String[][] getdata() throws IOException
+	{
+		String path = "./src\\test\\resources\\Data\\createReservationtestData.xlsx";
+		XLUtils xlutils = new XLUtils(path);
+		
+		int totalrowcount = xlutils.getRowCount("Sheet1");
+		int totalcolcount = xlutils.getCellCount("Sheet1", 1);
+		
+		String createReservationtestData[][] = new String[totalrowcount][totalcolcount];
+		
+		for(int i=1;i<=totalrowcount;i++)
+		{
+			for(int j=0;j<totalcolcount;j++)
+			{
+				createReservationtestData[i-1][j]=xlutils.getCellData("Sheet1", i, j);
+			}
+			
+		}
+		
+		return createReservationtestData;
 		
 	}
 	
